8e85bc4319aa022f4348ea8873dedff92700f708
Murray Tannock <murraytannock@gmail.com>
2016-11-28 15:29:51 +0000

    Blog page with Jake Weary filters, Also realized I needed Jake Weary for menu to work

--- js/main.js
+++ js/main.js
@@ -17,10 +17,11 @@ $(window).resize(function() {
   }
 });
 
+$( ".filter-item" ).on("click", function(e){$(this).toggleClass("active");});//.css( "border", "3px solid red" );
+
 $(document).ready(function() {
   var menuToggle = $("#js-mobile-menu").unbind();
   $("#js-navigation-menu").removeClass("show");
-
   menuToggle.on("click", function(e) {
     e.preventDefault();
     $("#js-navigation-menu").slideToggle(function(){
@@ -29,4 +30,19 @@ $(document).ready(function() {
       }
     });
   });
-}); 
+
+    $( ".filter-item" ).on("click", function(e){
+      $( ".filter-item").not(this).removeClass("active");
+      if ($(this).hasClass("active")){
+        // Deactivating the class should show all posts
+        $(this).removeClass("active");
+        $( ".grid-item").show();
+      } else {
+        $(this).addClass("active");
+        var classes = $(this).attr("class").split(/\s+/);
+        var cat = "." + classes[1].substring(7);
+        $( ".grid-item ").show()
+        $( ".grid-item").not(cat).hide();
+      };
+    });
+});
